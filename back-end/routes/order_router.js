559fb616d8e8d1fb0cd915344004a45fe6591be0
htlshu <285033715@qq.com>
2018-10-29 22:31:49 +0800

    ''

--- back-end/routes/order_router.js
+++ back-end/routes/order_router.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var orderController = require('../controllers/order_controller');
-
+//设置路由
 router.get('/list',orderController.list);
-
+router.get('/remove',orderController.remove);
+router.post('/save',orderController.save);
 module.exports = router;
